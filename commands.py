ee2ae6c682033b1e9fff2e85dec70390ed1b63bc
SwthSawTheme <sawtheme1@gmail.com>
2024-06-09 10:22:07 -0300

    Codigo refatorado

--- commands.py
+++ commands.py
@@ -1,3 +1,15 @@
 import discord
-from main import client
 
+async def hello(ctx: discord.ApplicationContext):
+    embed = discord.Embed(
+        title="Meus filhos...",
+        description="A salvação não está na luz, está em vocês!",
+        color=discord.Colour.red(),
+    )
+    
+    embed.set_footer(text="Os senhores do inferno estão vindo para devorar nosso mundo...")
+    embed.set_author(name="Filha do ódio", icon_url="https://assetsio.gnwcdn.com/diablo-4-max-level-lilith.jpg?width=1200&height=1200&fit=crop&quality=100&format=png&enable=upscale&auto=webp")
+    embed.set_thumbnail(url="https://static.wikia.nocookie.net/diablo/images/5/52/Lilith_DIV_face2.jpg/revision/latest/scale-to-width-down/1200?cb=20230328014303")
+    embed.set_image(url="https://images-ext-1.discordapp.net/external/c5eAZksU1MdpKAn9DyOxYjOZz_DdELNpj-pFF5W7uF8/https/sucodemanga.com.br/wp-content/uploads/2023/03/lilith-diablo-4-primeiro-gole.jpg?format=webp&width=1195&height=670")
+ 
+    await ctx.respond(embed=embed)  # Enviar o embed com algum texto
--- main.py
+++ main.py
@@ -1,26 +1,25 @@
 import os
 import discord
 from dotenv import load_dotenv
+import commands
 
+# Carregar variáveis de ambiente do arquivo .env
 load_dotenv()
-token = os.getenv("token")
 
+# Definir intents
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+# Criar uma instância do bot com intents especificadas
+bot = discord.Bot(intents=intents)
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'{client.user} caminha por santuario...')
-    
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+    print(f"{bot.user} Caminha por santuario...")
 
-    if message.content.startswith('/Olá'):
-        await message.channel.send(f'olá {message.author}')
+@bot.slash_command(name="benca", description="Peça bença a mãe...")
+async def benca(ctx: discord.ApplicationContext):
+    await commands.hello(ctx)
 
-client.run(token)
-
+# Executar o bot
+bot.run(os.getenv('TOKEN'))
